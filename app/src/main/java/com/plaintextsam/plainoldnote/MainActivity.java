b0af948b9d8d5cfb4fc7df4630a6c1321bb3c50a
sarath.kn <sarath.kn@robosoftin.com>
2018-11-15 15:37:32 +0530

    added list item

--- app/src/main/java/com/plaintextsam/plainoldnote/MainActivity.java
+++ app/src/main/java/com/plaintextsam/plainoldnote/MainActivity.java
@@ -1,31 +1,40 @@
 package com.plaintextsam.plainoldnote;
 
+import android.databinding.DataBindingUtil;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import com.plaintextsam.plainoldnote.databinding.ActivityMainBinding;
+import com.plaintextsam.plainoldnote.databinding.ContentMainBinding;
+
+import io.reactivex.Observable;
+
 public class MainActivity extends AppCompatActivity {
+    ActivityMainBinding activityMainBinding;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        activityMainBinding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+        Toolbar toolbar = activityMainBinding.toolbar;
         setSupportActionBar(toolbar);
+        FloatingActionButton fab = activityMainBinding.fab;
+        initRecyclerView();
+        fab.setOnClickListener(view -> Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                .setAction("Action", null).show());
+    }
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+    private void initRecyclerView() {
+        activityMainBinding.included.recyclerView.setHasFixedSize(true);
+        activityMainBinding.included.recyclerView.setLayoutManager(new LinearLayoutManager(this));
     }
 
     @Override
@@ -46,7 +55,6 @@ public class MainActivity extends AppCompatActivity {
         if (id == R.id.action_settings) {
             return true;
         }
-
         return super.onOptionsItemSelected(item);
     }
 }
