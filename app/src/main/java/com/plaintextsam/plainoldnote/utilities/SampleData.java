52fbba2f1beb282fe22b4bbdb2fbfa23118dbd10
sarath.kn <sarath.kn@robosoftin.com>
2018-11-15 15:43:42 +0530

    added sample data

---
+++ app/src/main/java/com/plaintextsam/plainoldnote/utilities/SampleData.java
@@ -0,0 +1,8 @@
+package com.plaintextsam.plainoldnote.utilities;
+
+public class SampleData {
+
+    public static final String SAMPLE_TEXT_1 = "A Sample Note";
+    public static final String SAMPLE_TEXT_2 = "A note with a\nline feed";
+    public static final String SAMPLE_TEXT_3 = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.";
+}
